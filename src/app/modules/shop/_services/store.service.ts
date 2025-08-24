4485e9eb06687608bf51488d6e42729f7e85ff26
PiusAsh <pius1ash@gmail.com>
2025-08-24 09:23:37 +0100

    update

--- src/app/modules/shop/_services/store.service.ts
+++ src/app/modules/shop/_services/store.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../../public/environment/environment';
 
@@ -13,16 +13,19 @@ export class StoreService {
   constructor(private http: HttpClient) { }
 
   logPendingPayment(payload: any): Observable<any> {
-    return this.http.post(`${this.baseUrl}Payments/log-pending`, payload);
+    const headers = new HttpHeaders().set('Skip-Alert', 'true');
+    return this.http.post(`${this.baseUrl}Payments/log-pending`, payload, { headers });
   }
 
   updatePaymentStatus(transactionReference: string, payload: any): Observable<any> {
-    return this.http.put(`${this.baseUrl}Payments/update-status/${transactionReference}`, payload);
+    const headers = new HttpHeaders().set('Skip-Alert', 'true');
+    return this.http.put(`${this.baseUrl}Payments/update-status/${transactionReference}`, payload, { headers });
   }
 
   checkout(orderPayload: any): Observable<any> {
+    const headers = new HttpHeaders().set('Skip-Alert', 'true');
     // This method will be called from the checkout page to send the order details
     // and initiate the payment process.
-    return this.http.post(`${this.baseUrl}Order`, orderPayload);
+    return this.http.post(`${this.baseUrl}Order`, orderPayload, { headers });
   }
 }
