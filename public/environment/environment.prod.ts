c5d52efb28655eea316c6481b42653b8f96bc816
PiusAsh <pius1ash@gmail.com>
2025-08-28 02:09:40 +0100

    update

--- public/environment/environment.prod.ts
+++ public/environment/environment.prod.ts
@@ -1,10 +1,7 @@
 export const environment = {
   production: true,
-  // BASE_URL: 'https://server.bennyhosea.com/api/',
-  BASE_URL: 'https://localhost:7028/api/',
-
-  // PAYSTACK_KEY: 'pk_test_db114057f7e1e073f3bc5d5551869e8eef51b9b1',
-  PAYSTACK_KEY: 'pk_test_bc60a6e80c8aff6b1d63c2ab5fd789790f9be936',
+  BASE_URL: 'https://server.bennyhosea.com/api/',
+  PAYSTACK_KEY: 'pk_live_99d46492b1efd851a51ff1169ffd65d7f1c7f556',
   google_Client:
     'http://948475767702-6ib3iq44k364pie2vibc0o1o59n8io57.apps.googleusercontent.com',
 };
